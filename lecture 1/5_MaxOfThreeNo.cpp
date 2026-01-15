3216a859194161f02937083e9452faf094b4e158
Ashu <ashutosh.mihra.royal.max@gmail.com>
2026-01-15 13:59:51 +0530

    Adding the Max of threee no Program

--- lecture 1/5_MaxOfThreeNo.cpp
+++ lecture 1/5_MaxOfThreeNo.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 int main()
 {
+    int no_1;
+    int no_2;
+    int no_3;
+    cin >> no_1 >> no_2 >> no_3;
+
+    if (no_1 != no_2 && no_3 != no_1 && no_3 != no_1)
+    {
+        if (no_1 < no_2 && no_3 < no_2)
+        {
+            cout << no_2 << " is greater " << endl;
+        }
+        if (no_2 < no_3 && no_1 < no_3)
+        {
+            cout << no_3 << " is greater " << endl;
+        }
+        else
+        {
+            cout << no_1 << "is greater " << endl;
+        }
+    }
+    else
+    {
+        cout << "All are equal";
+    }
     return 0;
 }
